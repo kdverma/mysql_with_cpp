453d21ba971cbb0cc20a5b1b4e87f6d913933d95
root <root@btpvm2125.hpeswlab.net>
2019-12-18 11:58:06 +0530

    writter threads working but reader is pending

---
+++ databaseAPI.h
@@ -0,0 +1,239 @@
+#include <sstream>
+#include <vector>
+#include <thread>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <cstdint>
+#include <inttypes.h>
+#include <string.h>
+#include <mysql.h>
+#include "binaryblob.h"
+using namespace std;
+
+#define BILLION 1000000000L
+//#define RECORDS_PER_THREAD (3251041/12)
+#define RECORDS_PER_THREAD 1
+
+class FFError
+{
+public:
+    std::string Label;
+
+    FFError() { Label = (char *)"Generic Error"; }
+    FFError(char *message) { Label = message; }
+    ~FFError() {}
+    inline const char *GetMessage(void) { return Label.c_str(); }
+};
+
+void add_session(MYSQL *Conn, std::string &cacheId, uint64_t &resourceSize, uint64_t &resourceCreated, string &resourceHeader, string &resourceUrl)
+{
+    void *bin_data = rawDataOne;
+    size_t bin_size = 4425; //7; //4425; // -- ...and the size of the binary data
+
+    try
+    {
+        char chunk[2 * bin_size + 1];
+        mysql_real_escape_string(Conn, chunk, rawDataOne, bin_size);
+
+        char *st = "INSERT INTO cachedbtable (cache_id, resource_size, resource_created, resource_headers, resource_url, resource_data) VALUES ('%s',%lld,%lld,'%s','%s','%s');";
+        size_t st_len = strlen(st);
+
+        int totalSize = (st_len + (2 * bin_size) + (19 * 2) + cacheId.size() + resourceHeader.size() + resourceUrl.size());
+        char query[totalSize + 1];
+        int len = snprintf(query, (totalSize + 1), st, cacheId.c_str(), resourceSize, resourceCreated, resourceHeader.c_str(), resourceUrl.c_str(), chunk);
+
+        //printf("\nquery : [%s] len [%d]\n", query, strlen(query));
+        int mysqlStatus = mysql_real_query(Conn, query, totalSize + 1);
+        if (mysqlStatus)
+        {
+            throw FFError((char *)mysql_error(Conn));
+        }
+    }
+    catch (FFError e)
+    {
+        printf("\n1 ERROR while inserting data to db : %s\n", e.Label.c_str());
+        //return 1;
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "ERRRORRRRRRRRRRRRR" << endl;
+        std::cerr << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "ERRRORRRRRRRRRRRRR" << endl;
+    }
+}
+
+void generateRandomData(string &cacheId, uint64_t &resourceSize, uint64_t &resourceCreated, string &resourceHeader, string &resourceUrl, int &arg)
+{
+    long int ns;
+    time_t sec;
+    struct timespec spec;
+
+    clock_gettime(CLOCK_REALTIME, &spec);
+    sec = spec.tv_sec;
+    ns = spec.tv_nsec;
+
+    uint64_t randomNum = (uint64_t)sec * BILLION + (uint64_t)ns;
+
+    cacheId = "";
+    std::ostringstream oss;
+    oss << randomNum;
+    oss << "_" << arg;
+    cacheId.append(oss.str());
+
+    resourceSize = sec;
+    resourceCreated = sec;
+    resourceHeader = cacheId;
+    resourceUrl = cacheId;
+}
+
+void workerThreadInsert(int arg)
+{
+    MYSQL *MySQLConRet;
+    MYSQL *MySQLConnection = NULL;
+
+    string hostName = "localhost";
+    string userId = "root";
+    string password = "Google@123";
+    string DB = "cachedatabase";
+
+    MySQLConnection = mysql_init(NULL);
+
+    try
+    {
+        MySQLConRet = mysql_real_connect(MySQLConnection, hostName.c_str(), userId.c_str(), password.c_str(), DB.c_str(), 0, NULL, 0);
+
+        if (MySQLConRet == NULL)
+        {
+            throw FFError((char *)mysql_error(MySQLConnection));
+        }
+    }
+    catch (FFError e)
+    {
+        printf("\n2. ERROR while inserting data to db : %s\n", e.Label.c_str());
+    }
+
+    time_t startTime = time(0);
+    printf("writter thread [%d] started at [%d] for [%d] entries\n", arg, startTime, RECORDS_PER_THREAD);
+    try
+    {
+        //int mysqlStatus = 0;
+        //MYSQL_RES *mysqlResult = NULL;
+
+        int count = 0;
+        while (count < RECORDS_PER_THREAD)
+        {
+
+            std::string cacheId;
+            uint64_t resourceSize;
+            uint64_t resourceCreated;
+            string resourceHeader;
+            string resourceUrl;
+
+            generateRandomData(cacheId, resourceSize, resourceCreated, resourceHeader, resourceUrl, arg);
+            add_session(MySQLConnection, cacheId, resourceSize, resourceCreated, resourceHeader, resourceUrl);
+
+            /*sleep for 1/10 (100000 us) of second*/
+            //usleep(100000);
+            count++;
+        }
+        time_t endTime = time(0);
+        printf("writter thread [%d] stopped at [%d] after inserting [%d] rows, time taken = [%d] seconds\n", arg, endTime, RECORDS_PER_THREAD, (endTime - startTime));
+    }
+    catch (FFError e)
+    {
+        printf("\n3. ERROR while inserting data to db : %s\n", e.Label.c_str());
+        //return 1;
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "ERRRORRRRRRRRRRRRR" << endl;
+        std::cerr << e.what() << std::endl;
+        std::cerr << "thread number : " << arg << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "ERRRORRRRRRRRRRRRR" << endl;
+        std::cerr << "thread number : " << arg << std::endl;
+    }
+    mysql_thread_end();
+}
+
+void workerThreadRead(int arg)
+{
+#if 1
+    MYSQL *MySQLConRet;
+    MYSQL *MySQLConnection = NULL;
+
+    string hostName = "localhost";
+    string userId = "root";
+    string password = "Google@123";
+    string DB = "cachedatabase";
+
+    MySQLConnection = mysql_init(NULL);
+
+    try
+    {
+        MySQLConRet = mysql_real_connect(MySQLConnection, hostName.c_str(), userId.c_str(), password.c_str(), DB.c_str(), 0, NULL, 0);
+
+        if (MySQLConRet == NULL)
+        {
+            throw FFError((char *)mysql_error(MySQLConnection));
+        }
+    }
+    catch (FFError e)
+    {
+        printf("\n1. ERROR while reading data from db : %s\n", e.Label.c_str());
+    }
+
+
+
+    time_t startTime = time(0);
+    printf("reader thread [%d] started at [%d] for [%d] entries\n", arg, startTime, RECORDS_PER_THREAD);
+    try
+    {
+        int count = 0;
+        while (count < RECORDS_PER_THREAD)
+        {
+            std::string sessionId;
+            int pageSeq = 0;
+            std::string cacheId;
+            std::string eventId;
+
+            pqxx::work txn{C};
+            pqxx::result data = txn.exec("SELECT * FROM cachedbtable ORDER BY cache_id DESC limit 1");
+            if (data.size() != 1)
+            {
+                printf("thread [%d] Could not read data from from db [%s] \n", arg, C.dbname());
+            }
+            txn.commit();
+
+            /*sleep for 1/10 (100000 us) of second*/
+            //usleep(10000);
+            count++;
+        }
+        time_t endTime = time(0);
+        printf("reader thread [%d] stopped at [%d] after %d times read operation, time taken = [%d] seconds\n", arg, endTime, count, (endTime - startTime));
+    }
+    catch (FFError e)
+    {
+        printf("\n1 ERROR while inserting data to db : %s\n", e.Label.c_str());
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "ERRRORRRRRRRRRRRRR" << endl;
+        std::cerr << e.what() << std::endl;
+        std::cerr << "thread number : " << arg << std::endl;
+    }
+    catch (...)
+    {
+        std::cout << "ERRRORRRRRRRRRRRRR" << endl;
+        std::cerr << "thread number : " << arg << std::endl;
+    }
+#endif
+}
---
+++ main.cpp
@@ -0,0 +1,60 @@
+//#define _GLIBCXX_USE_CXX11_ABI 0
+#include <iostream>
+#include <thread>
+#include "databaseAPI.h"
+
+#define NUM_OF_THREAD 2
+int main()
+{
+
+    if (mysql_library_init(0, NULL, NULL))
+    {
+        fprintf(stderr, "could not initialize MySQL client library\n");
+        exit(1);
+    }
+    try
+    {
+        long id;
+        std::vector<std::thread> writterThreads;
+        std::vector<std::thread> readerThreads;
+        /*for (id = 1; id <= NUM_OF_THREAD; id++)
+        {
+            writterThreads.emplace_back(std::thread(workerThreadInsert, id));
+        }*/
+
+        /for (id = 1; id <= NUM_OF_THREAD; id++)
+        {
+            readerThreads.emplace_back(std::thread(workerThreadRead, id));
+        }
+
+        printf("Waiting for thread to finish...\n");
+
+        /*for (auto &t : writterThreads)
+        {
+            if (t.joinable())
+            {
+                t.join();
+            }
+        }*/
+
+        for (auto &t : readerThreads)
+        {
+            if (t.joinable())
+            {
+                t.join();
+            }
+        }
+    }
+    catch (FFError e)
+    {
+        printf("\nERROR : %s\n", e.Label.c_str());
+        //return 1;
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << "ERRRORRRRRRRRRRRRR" << endl;
+        std::cerr << e.what() << std::endl;
+    }
+    mysql_library_end();
+    return 0;
+}
